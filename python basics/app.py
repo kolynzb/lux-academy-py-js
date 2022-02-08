89536a3a74e5f30c5b610c6028b4d2b5aee74f8e
kolynzb <collinsbenda360@gmail.com>
2022-02-08 21:07:42 +0300

    thats wassap '

---
+++ python basics/app.py
@@ -0,0 +1,11 @@
+name = input('enter the age')
+
+age = input('enter the age')
+print(f'{name}  you are  {int(age)}') #interpolation 
+print('your name is ' , name) # concantenation
+
+#numeric literals
+yof = 1222
+#ghg= 3 + 4i
+
+# comments
